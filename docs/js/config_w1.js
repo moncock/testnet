7637eb040b3e28be3f000486182347250f20a603
diewland <diewland@gmail.com>
2025-01-25 14:25:47 +0700

    fork open-edition

---
+++ docs/js/config_w1.js
@@ -0,0 +1,21 @@
+// chain
+const CHAIN_NAME      = "Monad Devnet";
+const CHAIN_RPC       = "https://devnet1.monad.xyz/rpc/8XQAiNSsPCrIdVttyeFLC6StgvRNTdf";
+const CHAIN_ID        = 41454;
+const CHAIN_SYMBOL    = "DMON";
+const CHAIN_EXPLORER  = "https://brightstar-884.devnet1.monad.xyz";
+
+// contract
+const MAX_SUPPLY      = 10_000;
+const MINT_PER_WALLET = 2;
+const CONTRACT_ADDR   = "0xe32Dd61A7A62560c1caC21A149763537d09328E8";
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = null;
