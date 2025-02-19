04adf24b17b8914d4b8360dd7b9f3d9133b5852f
diewland <diewland@gmail.com>
2025-02-20 01:34:40 +0700

    update open-edition config

--- docs/js/config_oe.js
+++ docs/js/config_oe.js
@@ -1,14 +1,14 @@
 // chain
-const CHAIN_NAME      = "Sepolia Testnet";
-const CHAIN_RPC       = "https://eth-sepolia.g.alchemy.com/v2/cN878CC_klVwsO9QsJ9mlLRqxKBeNHIU";
-const CHAIN_ID        = 11155111;
-const CHAIN_SYMBOL    = "ETH";
-const CHAIN_EXPLORER  = "https://sepolia.etherscan.io";
+const CHAIN_NAME      = "Monad Testnet";
+const CHAIN_RPC       = "https://monad-testnet.g.alchemy.com/v2/cN878CC_klVwsO9QsJ9mlLRqxKBeNHIU";
+const CHAIN_ID        = 10143;
+const CHAIN_SYMBOL    = "MON";
+const CHAIN_EXPLORER  = "https://monad-testnet.socialscan.io";
 
 // contract
 const MAX_SUPPLY      = null;
 const MINT_PER_WALLET = null;
-const CONTRACT_ADDR   = "0x136325681B0Ca8cAB9CEF32dfa47bDBAAe2C1BC9";
+const CONTRACT_ADDR   = "0xc1afded3a96adcfd5d2148ddd54a2db95f770b19";
 const MINT_GAS_RATE   = 1;
 const MINT_PRICE      = 0;
 const TOKEN_SYMBOL    = null;
