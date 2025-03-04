edba0c5369919936d318d52f1c0e38f9bfadcaf9
diewland <diewland@gmail.com>
2025-03-04 22:48:55 +0700

    prepare 100k site

---
+++ docs/js/config_100k.js
@@ -0,0 +1,23 @@
+// chain
+const CHAIN_NAME      = "Monad Testnet";
+const CHAIN_RPC       = "https://testnet-rpc.monad.xyz";
+const CHAIN_ID        = 10143;
+const CHAIN_SYMBOL    = "MON";
+const CHAIN_EXPLORER  = "https://testnet.monadexplorer.com/";
+
+// contract
+const MAX_SUPPLY      = null;
+const MINT_PER_WALLET = 1;
+const CONTRACT_ADDR   = "0xc1afded3a96adcfd5d2148ddd54a2db95f770b19"; // TODO
+const MINT_GAS_RATE   = 1;
+const MINT_PRICE      = 0;
+const TOKEN_SYMBOL    = null;
+const TOKEN_ADDR      = null;
+
+// whitelist
+const PROOF_URL       = null;
+
+// twitter
+const TWEET_TEXT      = `Let's celebrate the minting of 100,000 Moncock OG NFTs on Monad Testnet 🎉
+
+https://testnet.moncock.xyz/100k`;
