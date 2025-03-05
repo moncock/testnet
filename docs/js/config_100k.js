25d47ea508adb16412d5b647fb5cdd79ebea0774
diewland <diewland@gmail.com>
2025-03-05 12:32:27 +0700

    update 100k contract

--- docs/js/config_100k.js
+++ docs/js/config_100k.js
@@ -8,7 +8,7 @@ const CHAIN_EXPLORER  = "https://testnet.monadexplorer.com/";
 // contract
 const MAX_SUPPLY      = null;
 const MINT_PER_WALLET = 1;
-const CONTRACT_ADDR   = "0x33F469E2dFa941674Ea1a38399FF43231FF5bc13";
+const CONTRACT_ADDR   = "0x661be01DE6C4b396231CF441E3a0f4630Cd31d65";
 const MINT_GAS_RATE   = 1;
 const MINT_PRICE      = 0;
 const TOKEN_SYMBOL    = null;
